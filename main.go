aaaa1c99086fb67b64ba2f4f33f38eba4359ed68
alejandroromano <alejandro.romano.meneses@gmail.com>
2020-05-22 13:42:54 -0500

    crud golang

--- main.go
+++ main.go
@@ -2,16 +2,105 @@ package main
 
 import (
 	"GolangNorthtwindRestApiu/database"
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
-	databaseConnection := database.InitDB()
+var databaseConnection *sql.DB
 
-	//Logica
+type Product struct {
+	ID           int    `json:"id"`
+	Product_Code string `json:"product_code"`
+	Description  string `json:"description"`
+}
+
+func catch(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	databaseConnection = database.InitDB()
 
 	defer databaseConnection.Close()
-	fmt.Println(databaseConnection)
+
+	r := chi.NewRouter()
+
+	r.Get("/products", AllProductos)
+	r.Post("/products", CreateProducto)
+	r.Put("/products/{id}", UpdateProducto)
+	r.Delete("/products/{id}", DeleteProducto)
+	http.ListenAndServe(":3000", r)
+
+}
+
+func DeleteProducto(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	query, err := databaseConnection.Prepare("delete from products  where id=?")
+	catch(err)
+
+	_, er := query.Exec(id)
+	catch(er)
+	defer query.Close()
+
+	respondwithJson(w, http.StatusOK, map[string]string{"message": "delete successfully"})
+}
+
+func UpdateProducto(w http.ResponseWriter, r *http.Request) {
+	var product Product
+	id := chi.URLParam(r, "id")
+	json.NewDecoder(r.Body).Decode(&product)
+	query, err := databaseConnection.Prepare("Update products SET product_code=?, description=? where id=?")
+	catch(err)
+
+	_, er := query.Exec(product.Product_Code, product.Description, id)
+	catch(er)
+	defer query.Close()
+
+	respondwithJson(w, http.StatusOK, map[string]string{"message": "update successfully"})
+}
+
+func CreateProducto(w http.ResponseWriter, r *http.Request) {
+	var producto Product
+	json.NewDecoder(r.Body).Decode(&producto)
+	query, err := databaseConnection.Prepare("Insert products SET product_code=?, description=?")
+	catch(err)
+
+	_, er := query.Exec(producto.Product_Code, producto.Description)
+	catch(er)
+	defer databaseConnection.Close()
+
+	respondwithJson(w, http.StatusCreated, map[string]string{"message": "successfully created"})
+}
+
+func AllProductos(w http.ResponseWriter, r *http.Request) {
+	const sql = `SELECT id,product_code,COALESCE(description,'')
+			 FROM products`
+
+	results, err := databaseConnection.Query(sql)
+	catch(err)
+	var products []*Product
+
+	for results.Next() {
+		product := &Product{}
+		err = results.Scan(&product.ID, &product.Product_Code, &product.Description)
+		catch(err)
+		products = append(products, product)
+	}
+	respondwithJson(w, http.StatusOK, products)
+}
+
+func respondwithJson(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	fmt.Println(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
 }
